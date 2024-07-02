7e7915e05f0b0f16b30710f1c92a78121fd43302
xiidot1303 <xii1303@inbox.ru>
2024-07-02 14:59:20 +0500

    some changes

--- bot/control/handlers.py
+++ bot/control/handlers.py
@@ -8,9 +8,14 @@ from telegram.ext import (
     ConversationHandler
 )
 
+from bot.resources.strings import lang_dict
+from bot.resources.conversationList import *
+
 from bot.bot import (
     main,
 )
+
+exceptions_for_filter_text = (~filters.COMMAND) & (~filters.Text(lang_dict['main menu']))
 
 start = CommandHandler('start', main.start)
 
