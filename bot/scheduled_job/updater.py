b543bb22afe78103f90815b1393e2b5673d84262
xiidot1303 <xii1303@inbox.ru>
2024-07-12 17:51:21 +0500

    fix last commit

--- bot/scheduled_job/updater.py
+++ bot/scheduled_job/updater.py
@@ -1,6 +1,6 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from django_apscheduler.jobstores import register_events, DjangoJobStore
-from app.scheduled_job import mailing
+from bot.scheduled_job import mailing
 from asgiref.sync import async_to_sync
 
 class jobs:
